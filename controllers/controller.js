7bf513b103ac17f8bee380b4e2bc0bcd433eae62
ednguyen <ednguyen92@gmail.com>
2018-07-08 02:15:08 -0700

    Able to add Notes last need to display notes

--- controllers/controller.js
+++ controllers/controller.js
@@ -83,9 +83,10 @@ router.get("/clearAll", function(req, res) {
     });
 });
 
-router.post("/articles/save/:id", function(req, res) {
+router.post("/saved/:id", function(req, res) {
   // Use the article id to find and update its saved boolean
-  Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true})
+  Article.findOneAndUpdate({ _id: req.params.id }, { saved: true})
+  .populate("note")
   .then(function(dbArticle) {
     res.redirect('/');
   })
@@ -95,7 +96,69 @@ router.post("/articles/save/:id", function(req, res) {
   });
 });
 
+router.post("/unsaved/:id", function(req, res) {
+  // Use the article id to find and update its saved boolean
+  Article.findOneAndUpdate({ _id: req.params.id }, { saved: false})
+  .then(function(dbArticle) {
+    res.redirect('/savedarticles');
+  })
+  .catch(function(err) {
+    // If an error occurred, send it to the client
+    res.json(err);
+  });
+});
 
+// Route for getting all Articles from the db
+router.get("/savedarticles", function(req, res) {
+  // Grab every document in the Articles collection
+  Article.find({saved: true})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.render('articles', {
+        dbArticle: dbArticle
+      });
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+router.post("/articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  Note.create(req.body)
+    .then(function(dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for getting all Articles from the db
+router.get("/savednotes", function(req, res) {
+  // Grab every document in the Articles collection
+  Note.find({ _id: req.params.id })
+    .then(function(dbNote) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbNote, {
+        dbNote: dbNote
+      });  
+
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
 
   
   module.exports = router;
